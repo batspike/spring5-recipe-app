e1919fc937157d617a9285fe1997238d14ac3e3d
Sam Lau <samlau.sam@gmail.com>
2020-08-04 18:08:57 +0800

    test Controller using MockMVC

--- src/test/java/com/samcancode/controllers/IndexControllerTest.java
+++ src/test/java/com/samcancode/controllers/IndexControllerTest.java
@@ -2,6 +2,9 @@ package com.samcancode.controllers;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -11,6 +14,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import com.samcancode.domain.Recipe;
@@ -52,5 +57,16 @@ class IndexControllerTest {
 		Set<Recipe> setInController = argumentCaptor.getValue();
 		assertEquals(2, setInController.size());
 	}
+	
+	@Test
+	void testMockMVC() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
+		mockMvc.perform(get("/"))
+			   .andExpect(status().isOk())
+			   .andExpect(view().name("index"));
+	}
+
+
 
 }
