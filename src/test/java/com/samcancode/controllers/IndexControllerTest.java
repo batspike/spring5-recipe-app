f132437d7567ed8aad815813f9d81520e5f33124
Sam Lau <samlau.sam@gmail.com>
2020-08-04 16:31:03 +0800

    using Mockito to test controller

---
+++ src/test/java/com/samcancode/controllers/IndexControllerTest.java
@@ -0,0 +1,38 @@
+package com.samcancode.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import com.samcancode.services.RecipeService;
+
+class IndexControllerTest {
+
+	@Mock
+	RecipeService recipeService;
+	
+	@Mock
+	Model model;
+	
+	IndexController controller;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		controller = new IndexController(recipeService);
+	}
+
+	@Test
+	void getIndexPage() throws Exception {
+		String viewName = controller.getIndexPage(model);
+		assertEquals("index", viewName);
+		verify(recipeService, times(1)).getRecipes();
+		verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+	}
+
+}
