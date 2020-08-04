029fea39675af87e9f94b14048547cc057e2a9a4
Sam Lau <samlau.sam@gmail.com>
2020-08-04 17:52:18 +0800

    using Mockito to test Controller

--- src/test/java/com/samcancode/controllers/IndexControllerTest.java
+++ src/test/java/com/samcancode/controllers/IndexControllerTest.java
@@ -3,12 +3,17 @@ package com.samcancode.controllers;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import com.samcancode.domain.Recipe;
 import com.samcancode.services.RecipeService;
 
 class IndexControllerTest {
@@ -29,10 +34,23 @@ class IndexControllerTest {
 
 	@Test
 	void getIndexPage() throws Exception {
+		Set<Recipe> recipes = new HashSet<>();
+		recipes.add(new Recipe());
+		recipes.add(new Recipe());
+
+		when(recipeService.getRecipes()).thenReturn(recipes);
+		
+		@SuppressWarnings("unchecked")
+		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+		
 		String viewName = controller.getIndexPage(model);
 		assertEquals("index", viewName);
 		verify(recipeService, times(1)).getRecipes();
 		verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+		
+		verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+		Set<Recipe> setInController = argumentCaptor.getValue();
+		assertEquals(2, setInController.size());
 	}
 
 }
