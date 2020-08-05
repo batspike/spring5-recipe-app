1bfbfa74e5ddbf5de45da8d84d15f8c25eb677ac
Sam Lau <samlau.sam@gmail.com>
2020-08-05 15:33:10 +0800

    configure for maven failsave plugin for integration test.

--- src/test/java/com/samcancode/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/samcancode/repositories/UnitOfMeasureRepositoryIT.java
@@ -5,13 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.MethodMode;
+
 import com.samcancode.domain.UnitOfMeasure;
 
 @SpringBootTest
-class UnitOfMeasureRepositoryTest {
+@TestMethodOrder(OrderAnnotation.class)
+class UnitOfMeasureRepositoryIT {
 
 	@Autowired
 	UnitOfMeasureRepository uomRepo;
@@ -21,12 +28,15 @@ class UnitOfMeasureRepositoryTest {
 	}
 
 	@Test
+	@Order(1)
+	@DirtiesContext(methodMode=MethodMode.AFTER_METHOD)//reset context after current method completed.
 	void findByDescriptionTeaSpoon() {
 		Optional<UnitOfMeasure> uomOptional = uomRepo.findByDescription("Teaspoon");
 		assertEquals("Teaspoon", uomOptional.get().getDescription());
 	}
 
 	@Test
+	@Order(2)
 	void findByDescriptionCup() {
 		Optional<UnitOfMeasure> uomOptional = uomRepo.findByDescription("Cup");
 		assertEquals("Cup", uomOptional.get().getDescription());
