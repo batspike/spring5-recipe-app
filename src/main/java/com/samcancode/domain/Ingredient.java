e054934021737b59a7c2f789edf2c7f836790206
Sam Lau <samlau.sam@gmail.com>
2020-08-24 22:08:15 +0800

    refactored db init for MySql and profile

--- src/main/java/com/samcancode/domain/Ingredient.java
+++ src/main/java/com/samcancode/domain/Ingredient.java
@@ -3,39 +3,49 @@ package com.samcancode.domain;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
-@Data
-@NoArgsConstructor
 public class Ingredient {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
-	
-	private String description;
-	private BigDecimal amount;
-	
-	@OneToOne
-	private UnitOfMeasure uom;
-	
-	@ManyToOne
-	private Recipe recipe;
 
-	public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
-		this.description = description;
-		this.amount = amount;
-		this.uom = uom;
-		this.recipe = recipe;
-	}
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+
+    @ManyToOne
+    private Recipe recipe;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+        this.recipe = recipe;
+    }
+
 }
